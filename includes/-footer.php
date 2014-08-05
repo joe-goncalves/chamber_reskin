096969abb46594dc062f14a15b888f010eddadfa
joegoncalves <joe.a.goncalves@gmail.com>
2014-08-05 18:44:36 -0400

    finished footer

---
+++ includes/-footer.php
@@ -0,0 +1,37 @@
+<div id="footer" class='container-fluid'>
+    <div class="row">
+        <div class="col-md-6">
+            <h2>The Chamber of Commerce of the Greater Ronkonkomas</h2>
+            <table class='table'>
+                <tr>
+                    <td>Contact Us:&nbsp;PO Box 2546</td>
+                    <td>p: 631-963-2796</td>
+                </tr>
+                <tr>
+                    <td>Ronkonkoma, NY 11779</td>
+                    <td>e:  <a class="foot_links" href="mailto:info@ronkonkomachamber.com">info@ronkonkomachamber.com</a></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-md-4">
+            <h2>join our mailing list</h2>
+            <form method="post" target="_blank" id='ccoptin' action="http://visitor.constantcontact.com/d.jsp" name="ccoptin">
+              Email:<input type="text" value="" size="26" name="ea" style="margin-left:4px;">
+              <input id = "cc_email_join" type="submit" class="submit" value="Join" name="go">
+              <input type="hidden" value="1102554981432" name="m">
+              <input type="hidden" value="oi" name="p">
+            </form>
+        </div>
+        <div class ="col-md-2">
+            <a style="color:#fff" href="http://www.facebook.com/pages/Ronkonkoma-Chamber-of-Commerce/193799817361633">
+                <img style="margin-right:5px" alt="FB" src="http://www.ronkonkomachamber.com/images/fb.png">
+                STAY CONNECTED<br>
+                <span>like us on facebook</span>
+            </a>
+        </div>
+    </div>
+    <p class="footnotes">Copyright &copy; 2013. The Chamber of Commerce of the Great Ronkonkomas</p>
+</div>
+
+
+
